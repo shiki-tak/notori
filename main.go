88339425efae039284339fb0d9f93d2f8e6ff917
shiki.takahashi <shiki.takahashi@linecorp.com>
2020-05-16 15:42:15 +0900

    rm point type

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 // For json data
 type Plan struct {
 	Base   ExBase  `json:"base"`
-	Routes []Route `json:"Routes"`
-	Costs  []Cost  `json:"Costs"`
+	Routes []Route `json:"routes"`
+	Costs  []Cost  `json:"costs"`
 }
 
 type Route struct {
@@ -25,31 +25,27 @@ type Cost struct {
 
 // For excel data
 type ExBase struct {
-	Id          int    `josn:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Period      int    `json:"period"`
+	Id          int
+	Title       string
+	Description string
+	Period      int
 }
 
 type ExRoute struct {
-	Id         int `json:"id"`
-	CourseTime int `json:"course_time"`
-	BaseId     int `json:"base_id"`
-}
-
-type ExPoint struct {
-	RouteId int    `json:"route_id"`
-	Name    string `json:"point"`
+	Id         int
+	CourseTime int
+	Points     []string
+	BaseId     int
 }
 
 type ExCost struct {
-	ExBaseId int    `json:"id"`
-	Usecase  string `json:"usecase"`
-	Price    int    `json:"price"`
+	ExBaseId int
+	Usecase  string
+	Price    int
 }
 
 func main() {
-	exBase, exRoutes, exPoins, exCosts := CreateSamplePlans()
+	exBase, exRoutes, exCosts := CreateSamplePlans()
 
 	plans := []Plan{}
 	for _, exb := range exBase {
@@ -59,14 +55,10 @@ func main() {
 		// for route
 		for _, exr := range exRoutes {
 			route := Route{}
-			id := exr.BaseId
-			if id == exb.Id {
+			if exr.BaseId == exb.Id {
 				route.CourseTime = exr.CourseTime
-				for _, exp := range exPoins {
-					if exr.Id == exp.RouteId {
-						route.Points = append(route.Points, exp.Name)
-					}
-				}
+				route.Points = exr.Points
+
 				plan.Routes = append(plan.Routes, route)
 			}
 		}
@@ -103,30 +95,21 @@ func main() {
 	fmt.Println(indentJSON)
 }
 
-func CreateSamplePlans() ([]ExBase, []ExRoute, []ExPoint, []ExCost) {
+func CreateSamplePlans() ([]ExBase, []ExRoute, []ExCost) {
 
 	// for sample plans1
 	b1 := ExBase{0, "sample_title1", "sample_description1", 2}
-	r1 := ExRoute{0, 10, 0}
-	r2 := ExRoute{1, 9, 0}
-	p1 := ExPoint{0, "sp1"}
-	p2 := ExPoint{0, "sp2"}
-	p3 := ExPoint{0, "sp3"}
-	p4 := ExPoint{1, "sp4"}
-	p5 := ExPoint{1, "sp5"}
+	r1 := ExRoute{0, 10, []string{"sp1", "sp2", "sp3"}, 0}
+	r2 := ExRoute{1, 9, []string{"sp4", "sp5"}, 0}
 	c1 := ExCost{0, "sample_usecase1", 1000}
 	c2 := ExCost{0, "sample_usecase2", 2000}
 
 	// for sample plans2
 	b2 := ExBase{1, "sample_title2", "sample_description2", 1}
-	r3 := ExRoute{2, 8, 1}
-	p6 := ExPoint{2, "sp6"}
-	p7 := ExPoint{2, "sp7"}
-	p8 := ExPoint{2, "sp8"}
-	p9 := ExPoint{2, "sp9"}
+	r3 := ExRoute{2, 8, []string{"sp6", "sp7", "sp8", "sp9"}, 1}
 	c3 := ExCost{1, "sample_usecase3", 3000}
 	c4 := ExCost{1, "sample_usecase4", 4000}
 	c5 := ExCost{1, "sample_usecase5", 5000}
 
-	return []ExBase{b1, b2}, []ExRoute{r1, r2, r3}, []ExPoint{p1, p2, p3, p4, p5, p6, p7, p8, p9}, []ExCost{c1, c2, c3, c4, c5}
+	return []ExBase{b1, b2}, []ExRoute{r1, r2, r3}, []ExCost{c1, c2, c3, c4, c5}
 }
